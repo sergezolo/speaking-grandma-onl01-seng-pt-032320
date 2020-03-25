1d80fd964adb13490491a3358bbc5f9d5695e3cf
Sergey Zolotukhin <zolotukhinsereg@gmail.com>
2020-03-25 22:04:42 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,22 @@
 def speak_to_grandma(phrase)
+<<<<<<< HEAD
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
+=======
+  if phrase = phrase.upcase
+    return "NO, NOT SINCE 1938!" 
+  elsif phrase = "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+>>>>>>> bcbf6ef7ad7eba79ece1dd955cfe4c863d7efbb5
   else 
     return "HUH?! SPEAK UP, SONNY!"
   end
 end
+<<<<<<< HEAD
  
+=======
+  
+
+>>>>>>> bcbf6ef7ad7eba79ece1dd955cfe4c863d7efbb5
